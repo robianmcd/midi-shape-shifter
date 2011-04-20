563b382f152a3136e865a05ec4f737249808baed
robcheese@gmail.com <robcheese@gmail.com@4e31dc51-3177-9f86-f460-8117e2103d03>
2011-04-20 21:57:19 +0000

    Moved MappingEntry class to its own file
    git-svn-id: https://midi-shape-shifter.googlecode.com/svn/trunk@36 4e31dc51-3177-9f86-f460-8117e2103d03

---
+++ MidiShapeShifter/MidiShapeShifter/MappingEntry.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MidiShapeShifter
+{
+    public class MappingEntry : ICloneable
+    {
+        public MidiHelper.MidiMsgRange inMsgRange;
+        public MidiHelper.MidiMsgRange outMsgRange;
+
+        //If there are multiple mapping entries with overlapping input ranges then a single midi message can 
+        //generate several messages. This can be disabled by setting the override duplicates flag to true
+        public bool overrideDuplicates;
+
+        //specifies the order in which the override duplicates flags are considered. For example if there are two
+        //mapping entries with an overlapping inMsgRange and overrideDuplicates is set to true, then the one 
+        //with a priority closer to 0 will override the other. This priority also corresponds to the index of an 
+        //entry in the listbox it is displayed in. Each entry should have a unique priority.
+        public int priority;
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        public MappingEntry Clone()
+        {
+            return (MappingEntry)this.MemberwiseClone();
+        }
+
+
+        public string GetReadableMsgType(bool input)
+        {
+            if (input == true)
+            {
+                return MidiHelper.MidiMsgTypeStr[(int)this.inMsgRange.msgType];
+            }
+            else
+            {
+                return MidiHelper.MidiMsgTypeStr[(int)this.outMsgRange.msgType];
+            }
+        }
+
+        public string GetReadableChannelRange(bool input)
+        {
+            MidiHelper.MidiMsgRange range;
+            if (input == true)
+            {
+                range = this.inMsgRange;
+            }
+            else
+            {
+                range = this.outMsgRange;
+            }
+
+            if (range.bottomChannel == MidiHelper.RANGE_ALL_INT && range.topChannel == MidiHelper.RANGE_ALL_INT)
+            {
+                return MidiHelper.RANGE_ALL_STR;
+            }
+            else
+            {
+                return range.topChannel.ToString() + "-" + range.bottomChannel.ToString();
+            }
+        }
+
+        public string GetReadableParamRange(bool input)
+        {
+            MidiHelper.MidiMsgRange range;
+            if (input == true)
+            {
+                range = this.inMsgRange;
+            }
+            else
+            {
+                range = this.outMsgRange;
+            }
+
+            if (range.bottomParam == MidiHelper.RANGE_ALL_INT && range.topParam == MidiHelper.RANGE_ALL_INT)
+            {
+                return MidiHelper.RANGE_ALL_STR;
+            }
+            else
+            {
+                //TODO: The binding should also get a property changed notification on the ActiveParameter property, 
+                //indicating it should release the old instance and bind to the new.
+                return range.topParam.ToString() + "-" + range.bottomParam.ToString();
+            }
+        }
+
+        public string GetReadableOverrideDuplicates()
+        {
+            if (this.overrideDuplicates == true)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+    }
+}
