b8ea44424047454a70e88c004af984a32a1cd201
robcheese@gmail.com <robcheese@gmail.com@4e31dc51-3177-9f86-f460-8117e2103d03>
2011-04-20 21:51:16 +0000

    Added project for unit testing
    git-svn-id: https://midi-shape-shifter.googlecode.com/svn/trunk@33 4e31dc51-3177-9f86-f460-8117e2103d03

---
+++ MidiShapeShifter/MidiShapeShifterTest/MappingManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MidiShapeShifter;
+
+namespace MidiShapeShifterTest
+{
+    [TestFixture]
+    public class MappingManagerTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            MappingManager mappingMgr = new MappingManager();
+            MappingEntry mapEntry = new MappingEntry();
+
+            mapEntry.inMsgRange = new MidiHelper.MidiMsgRange();
+            mapEntry.inMsgRange.bottomParam = 1;
+            mapEntry.inMsgRange.topParam = 2;
+            mapEntry.inMsgRange.bottomChannel = 1;
+            mapEntry.inMsgRange.topChannel = 2;
+            mapEntry.inMsgRange.msgType = MidiHelper.MidiMsgType.NoteOn;
+
+            mapEntry.outMsgRange = new MidiHelper.MidiMsgRange();
+            mapEntry.outMsgRange.bottomParam = 1;
+            mapEntry.outMsgRange.topParam = 2;
+            mapEntry.outMsgRange.bottomChannel = 1;
+            mapEntry.outMsgRange.topChannel = 2;
+            mapEntry.outMsgRange.msgType = MidiHelper.MidiMsgType.NoteOn;
+
+            mapEntry.priority = 0;
+            mapEntry.overrideDuplicates = false;
+
+            Assert.IsTrue(mappingMgr.AddMappingEntry(mapEntry));
+        }
+    }
+}
