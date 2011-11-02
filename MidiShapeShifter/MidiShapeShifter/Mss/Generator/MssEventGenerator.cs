755899ad193014a3e0dcc5520fcbff7d4bfc6382
robcheese@gmail.com <robcheese@gmail.com@4e31dc51-3177-9f86-f460-8117e2103d03>
2011-11-02 20:38:28 +0000

    Added documentation, did some refactoring and fixed some small bugs.
    git-svn-id: https://midi-shape-shifter.googlecode.com/svn/trunk@100 4e31dc51-3177-9f86-f460-8117e2103d03

--- MidiShapeShifter/MidiShapeShifter/Mss/Generator/MssEventGenerator.cs
+++ MidiShapeShifter/MidiShapeShifter/Mss/Generator/MssEventGenerator.cs
@@ -9,7 +9,7 @@ using MidiShapeShifter.Mss.Mapping;
 
 namespace MidiShapeShifter.Mss.Generator
 {
-    // TODO: comment this calss and write test casses for it
+    // TODO: write test casses for this calss
     /// <summary>
     /// Before the end of each audio processing cycle this class will iterate through the 
     /// entries in generatorMappingManager, create all nessary Mss Events and send them to 
@@ -17,7 +17,7 @@ namespace MidiShapeShifter.Mss.Generator
     /// </summary>
     public class MssEventGenerator
     {
-        protected const int SAMPLES_PER_GENERATOR_UPDATE = 600;
+        protected const int SAMPLES_PER_GENERATOR_UPDATE = 200;
 
         /// <summary>
         /// This class sends generated events to this input port
@@ -39,12 +39,19 @@ namespace MidiShapeShifter.Mss.Generator
         /// Applies the equation the user has specified for each generator to generated events.
         /// </summary>
         protected MssMsgProcessor mssMsgProcessor;
-        
 
+        /// <summary>
+        /// Stores the sample time at the end of the last processing cycle. This should be updated at
+        /// the end of the ProcessingCycleEnd event.
+        /// </summary>
+        protected long sampleTimeAtEndOfLastCycle = 0;
+
+        //Constructor
         public MssEventGenerator()
         {
             this.mssMsgProcessor = new MssMsgProcessor();
         }
+
 
         public void Init(IHostInfoOutputPort hostInfoOutputPort, 
                          IWetMssEventOutputPort wetMssEventOutputPort, 
@@ -54,13 +61,16 @@ namespace MidiShapeShifter.Mss.Generator
             this.generatorMappingMgr = generatorMappingMgr;
             this.mssMsgProcessor.Init(generatorMappingMgr);
 
-            //Adds listener for generator toggle messages
+            //Adds listener for generator toggle messages.
             wetMssEventOutputPort.WetMssEventsReceived += new 
                     WetMssEventsReceivedEventHandler(WetMssEventOutputPort_WetMssEventsReceived);
 
             this.hostInfoOutputPort = hostInfoOutputPort;
+            
             hostInfoOutputPort.BeforeProcessingCycleEnd += new
                     ProcessingCycleEndEventHandler(HostInfoOutputPort_BeforeProcessingCycleEnd);
+            hostInfoOutputPort.ProcessingCycleEnd += new
+                    ProcessingCycleEndEventHandler(HostInfoOutputPort_ProcessingCycleEnd);
             
             this.dryMssEventInputPort = dryMssEventInputPort;
         }
@@ -85,7 +95,7 @@ namespace MidiShapeShifter.Mss.Generator
         /// each audio processing cycle ends. 
         /// </summary>
         /// <param name="sampleTimeAtEndOfCycle">
-        /// The sample time for when the current processing cycle will end
+        /// The sample time for when the current processing cycle will end.
         /// </param>
         protected void HostInfoOutputPort_BeforeProcessingCycleEnd(long sampleTimeAtEndOfCycle)
         {
@@ -108,6 +118,8 @@ namespace MidiShapeShifter.Mss.Generator
                         continue;
                     }
 
+                    //If the host stopped playing then nothing will be generated for this generator
+                    //and it will need to be reinitialized when the host starts playing again.
                     if (this.hostInfoOutputPort.TransportPlaying == false)
                     {
                         curEntry.GenHistoryInfo.Initialized = false;
@@ -115,21 +127,25 @@ namespace MidiShapeShifter.Mss.Generator
                     }
                 }
 
+                //Only enabled generators should generate anything
                 if (curEntry.GenConfigInfo.Enabled == true)
                 {
+                    //Initializes the history info for this generator if it has not already been
+                    //initialized.
                     if (curEntry.GenHistoryInfo.Initialized == false)
                     {
                         //The GenHistoryInfo will be initialized such that it appears to have been
-                        //updated on the last update.
-                        long sampleTimeOfLastUpdate = 
-                                sampleTimeAtEndOfCycle - SAMPLES_PER_GENERATOR_UPDATE;
-
+                        //updated on the last processing cycle.
                         curEntry.GenHistoryInfo.InitAllMembers(
-                                sampleTimeOfLastUpdate,
+                                this.sampleTimeAtEndOfLastCycle,
                                 0,
                                 double.NaN);
                     }
 
+                    //Generate events for this generator until the next event coming from this 
+                    //generator would fall into the next audio processing cycle. The enabled status
+                    //of a generator can change in a call to GenerateEvent() so we need to ensure 
+                    //that this generator is still enabled.
                     while (curEntry.GenHistoryInfo.SampleTimeAtLastGeneratorUpdate + 
                            SAMPLES_PER_GENERATOR_UPDATE <= sampleTimeAtEndOfCycle && 
                            curEntry.GenConfigInfo.Enabled == true)
@@ -144,14 +160,26 @@ namespace MidiShapeShifter.Mss.Generator
             }
         }
 
+        /// <summary>
+        /// Each call to GenerateEvent() will generate the next event for genEntry. The 
+        /// next event's sample time will be set to SAMPLES_PER_GENERATOR_UPDATE more then the 
+        /// sample time for the event that was previously created for genEntry. Returns null if 
+        /// no event currently needs to be generated for this generator.
+        /// </summary>
+        /// <remarks>
+        /// Preconditions: genEntry is initilaized and enabled.
+        /// </remarks>
         protected MssEvent GenerateEvent(IGeneratorMappingEntry genEntry)
         {
+            Debug.Assert(genEntry.GenConfigInfo.Enabled == true);
             Debug.Assert(genEntry.GenHistoryInfo.Initialized == true);
 
+            //Stores the relative position through the period that the next event for genEntry 
+            //will occur.
             double relPosInPeriod;
             if (genEntry.GenConfigInfo.PeriodType == GenPeriodType.BeatSynced)
             {
-                relPosInPeriod = GetPercentThroughBeatSyncedPeriod(genEntry.GenConfigInfo,
+                relPosInPeriod = GetRelPosInBeatSyncedPeriod(genEntry.GenConfigInfo,
                         genEntry.GenHistoryInfo.SampleTimeAtLastGeneratorUpdate + 
                         SAMPLES_PER_GENERATOR_UPDATE);
             }
@@ -162,10 +190,8 @@ namespace MidiShapeShifter.Mss.Generator
                         ((double)SAMPLES_PER_GENERATOR_UPDATE) / ((double)periodSizeInSamples);
                 relPosInPeriod = genEntry.GenHistoryInfo.PercentThroughPeriodOnLastUpdate +
                                         RelativeperiodIncrement;
-                if (relPosInPeriod > 1)
-                {
-                    relPosInPeriod--;                    
-                }
+                //Remove the interger component of relPosInPeriod so that it is between 0 and 1.
+                relPosInPeriod = relPosInPeriod % 1;
             }
             else
             {
@@ -174,25 +200,32 @@ namespace MidiShapeShifter.Mss.Generator
                 return null;
             }
 
-                if (genEntry.GenConfigInfo.Loop == false && 
-                    relPosInPeriod < genEntry.GenHistoryInfo.PercentThroughPeriodOnLastUpdate)
-                {
-                    genEntry.GenConfigInfo.Enabled = false;
-                    genEntry.GenHistoryInfo.Initialized = false;
-                    return null;
-                }
+            //If this generator is not set to loop and it has finished one full period then disable it
+            //it and return null so that no more events are sent.
+            if (genEntry.GenConfigInfo.Loop == false && 
+                (relPosInPeriod < genEntry.GenHistoryInfo.PercentThroughPeriodOnLastUpdate ||
+                GetPeriodSizeInSamples(genEntry.GenConfigInfo) < SAMPLES_PER_GENERATOR_UPDATE))
+            {
+                genEntry.GenConfigInfo.Enabled = false;
+                genEntry.GenHistoryInfo.Initialized = false;
+                return null;
+            }
 
             MssMsg relPosMsg = CreateInputMsgForGenMappingEntry(genEntry, relPosInPeriod);
+            //Processing the relPosMsg should convert it into a Generator message and apply the 
+            //equation for this generator to it's data3.
             List<MssMsg> processedMsgList = this.mssMsgProcessor.ProcessMssMsg(relPosMsg);
 
+            //Sample time for new event.
             long updatedSampleTime = genEntry.GenHistoryInfo.SampleTimeAtLastGeneratorUpdate +
                                       SAMPLES_PER_GENERATOR_UPDATE;
+            //Update the generator's history info.
             genEntry.GenHistoryInfo.SampleTimeAtLastGeneratorUpdate = updatedSampleTime;
             genEntry.GenHistoryInfo.PercentThroughPeriodOnLastUpdate = relPosInPeriod;
-            
+
             //Count could equal 0 if data 3 has been mapped above 1.
             Debug.Assert(processedMsgList.Count <= 1);
-
+            //Don't bother sending the event if it is the same as the last one sent.
             if (processedMsgList.Count == 0 || 
                 processedMsgList[0].Data3 == genEntry.GenHistoryInfo.LastValueSent)
             {
@@ -200,6 +233,7 @@ namespace MidiShapeShifter.Mss.Generator
             }
             else
             {
+                //Initialize the fields in the new event and return it.
                 MssEvent generatedEvent = new MssEvent();
                 generatedEvent.mssMsg = processedMsgList[0];
                 generatedEvent.sampleTime = updatedSampleTime;
@@ -210,11 +244,18 @@ namespace MidiShapeShifter.Mss.Generator
             }
         }
 
+        /// <summary>
+        /// Returns the size of the period for genConfigInfo in samples
+        /// </summary>
+        /// <remarks>
+        /// Preconditions: sample rate must be initialized in hostInfoOutputPort. If
+        /// genConfigInfo is for a beat synced generator then the time signature must also be
+        /// initialized in hostInfoOutputPort.
+        /// </remarks>
         protected int GetPeriodSizeInSamples(GenEntryConfigInfo genConfigInfo)
         {
             if (genConfigInfo.PeriodType == GenPeriodType.BeatSynced)
             {
-                Debug.Assert(hostInfoOutputPort.CalculatedBarZeroIsInitialized == true);
                 Debug.Assert(hostInfoOutputPort.TimeSignatureIsInitialized == true);
                 Debug.Assert(hostInfoOutputPort.SampleRateIsInitialized == true);
 
@@ -222,7 +263,8 @@ namespace MidiShapeShifter.Mss.Generator
                     genConfigInfo.GetSizeOfBarsPeriod(hostInfoOutputPort.TimeSignatureNumerator,
                                                       hostInfoOutputPort.TimeSignatureDenominator);
 
-                double timeSig = (double)hostInfoOutputPort.TimeSignatureNumerator / hostInfoOutputPort.TimeSignatureDenominator;
+                double timeSig = (double)hostInfoOutputPort.TimeSignatureNumerator /
+                                 hostInfoOutputPort.TimeSignatureDenominator;
                 double beatsPerBar = timeSig / (1/4d);
                 double secondsPerBar = (beatsPerBar / hostInfoOutputPort.Tempo) * 60;
                 return (int)System.Math.Round(
@@ -243,6 +285,11 @@ namespace MidiShapeShifter.Mss.Generator
             }
         }
 
+        /// <summary>
+        /// Create the relitive position message that genEntry should generate given relPosInPeriod.
+        /// This message should match an entry in the GeneratorMappingManager so that when it is
+        /// processed by mssMsgProcessor a Generator message will be returned.
+        /// </summary>
         protected MssMsg CreateInputMsgForGenMappingEntry(IGeneratorMappingEntry genEntry, 
                                                           double relPosInPeriod)
         {
@@ -271,9 +318,14 @@ namespace MidiShapeShifter.Mss.Generator
             return relPosMsg;
         }
 
-        //Preconditions: genInfo refers to a beat synced generator. CalculatedBarZero and 
-        //TimeSignature have been initialized in hostInfoOutputPort
-        protected double GetPercentThroughBeatSyncedPeriod(GenEntryConfigInfo genInfo, 
+        /// <summary>
+        /// Get the relative position into the beat synced period for genInfo at sampleTime
+        /// </summary>
+        /// <remarks>
+        /// Preconditions: genInfo refers to a beat synced generator. CalculatedBarZero and 
+        /// TimeSignature have been initialized in hostInfoOutputPort.
+        /// </remarks>
+        protected double GetRelPosInBeatSyncedPeriod(GenEntryConfigInfo genInfo, 
                                                               long sampleTime)
         {
             Debug.Assert(genInfo.PeriodType == GenPeriodType.BeatSynced);
@@ -287,5 +339,14 @@ namespace MidiShapeShifter.Mss.Generator
                                             hostInfoOutputPort.TimeSignatureDenominator);
             return barPos % periodSizeInBars / periodSizeInBars;
         }
+
+        /// <summary>
+        /// Handle the ProcessingCycleEnd event.
+        /// </summary>
+        protected void HostInfoOutputPort_ProcessingCycleEnd(long sampleTimeAtEndOfCycle)
+        {
+            this.sampleTimeAtEndOfLastCycle = sampleTimeAtEndOfCycle;
+        }
+
     }
 }
--- MidiShapeShifter/MidiShapeShifter/Mss/Relays/HostInfoRelay.cs
+++ MidiShapeShifter/MidiShapeShifter/Mss/Relays/HostInfoRelay.cs
@@ -7,24 +7,52 @@ using System.Diagnostics;
 namespace MidiShapeShifter.Mss.Relays
 {
     /// <summary>
-    ///     Accepts information from the host and sends a messages notifying any subscribers of the information.
+    ///     Accepts information about the host and sends messages notifying any subscribers of 
+    ///     the information. The intended usage of this calss is that at the end of every 
+    ///     processing cycle the following sequence of actions will occur:
+    ///     1. StartUpdate() will be called 
+    ///     2. Any information about the host will be sent to this relay through the 
+    ///          Recieve...DuringUpdate() functions.
+    ///     3. FinishUpdate() will be called.
+    ///     4. TriggerProcessingCycleEnd() will be called with the sample time at the end of the 
+    ///          processing cycle.
     /// </summary>
     /// <remarks>
-    ///     This class is used to pass host information from the "Framework" namespace to the "Mss" namespace.
+    ///     This class is used to pass host information from the "Framework" namespace to the 
+    ///     "Mss" namespace.
     /// </remarks>
     public class HostInfoRelay : IHostInfoInputPort, IHostInfoOutputPort
     {
 
-
+        /// <summary>
+        /// Specifies wheather this relay is accepting updates or not. This will be set to true 
+        /// when StartUpdate() is called and set to false when FinishUpdate() is called.
+        /// </summary>
         protected bool CurrentlyUpdating = false;
+
+        /// <summary>
+        /// Specifies the types of host info that this relay recieved on the last update.
+        /// </summary>
         public HostInfoFields UpdatedFieldsInLastUpdate {get; private set;}
+
+        /// <summary>
+        /// Specifies the host info that has changed on the last update.
+        /// </summary>
         public HostInfoFields ChangedFieldsInLastUpdate {get; private set;}
 
+        /// <summary>
+        /// Event that is fired after an update of host info has finished.
+        /// </summary>
         public event HostUpdateFinishedEventHandler HostUpdateFinished;
 
-        //used to trigger any processing that needs to happen before a processing cycle ends.
+        /// <summary>
+        /// Used to trigger any processing that needs to happen before a processing cycle ends.
+        /// </summary>
         public event ProcessingCycleEndEventHandler BeforeProcessingCycleEnd;
-        //used to trigger any processing that should happen at the very end of a processing cycle.
+
+        /// <summary>
+        /// Used to trigger any processing that should happen at the very end of a processing cycle.
+        /// </summary>
         public event ProcessingCycleEndEventHandler ProcessingCycleEnd;
 
         public double SampleRate { get; private set; }
@@ -45,8 +73,13 @@ namespace MidiShapeShifter.Mss.Relays
         public event TransportPlayingChangedEventHandler TransportPlayingChanged;
 
 
-        public bool CalculatedBarZeroIsInitialized { get; private set; }
+        /// <summary>
+        /// The sample time that the transport started playing. This value is calculated off of the
+        /// current information in HostInfoRelay so if for example the tempo changed then so would
+        /// CalculatedBarZeroSampleTime.
+        /// </summary>
         public long CalculatedBarZeroSampleTime {get; private set;}
+        public bool CalculatedBarZeroIsInitialized { get; private set; }
         public event CalculatedBarZeroChangedEventHandler CalculatedBarZeroChanged;
         protected double samplesPerBar;
 
@@ -67,15 +100,32 @@ namespace MidiShapeShifter.Mss.Relays
             this.ChangedFieldsInLastUpdate = HostInfoFields.None;
         }
 
-        //Precondition: BarsPosIsInitialized is true.
+        /// <summary>
+        /// Gets the number of bars into the song at a given sampleTime. This value is only 
+        /// calculated off of the most recent information so the result may not be accurate
+        /// if sampleTime is less then it was at the end of the last processing cycle. For example
+        /// if the transport was playing, then not playing and then playing again, this functions 
+        /// would return a negative bar position for any sample times before the second time the
+        /// transport started playing.
+        /// </summary>
+        /// <remarks>
+        /// Precondition: CalculatedBarZeroIsInitialized is true. TransportPlayingIsInitialized is 
+        /// true and TransportPlaying is true.
+        /// </remarks>
         public double GetBarPosAtSampleTime(long sampleTime)
         {
             Debug.Assert(this.CalculatedBarZeroIsInitialized);
+            Debug.Assert(this.TransportPlayingIsInitialized);
+            Debug.Assert(this.TransportPlaying == true);
 
             long samplesSinceStart = sampleTime - CalculatedBarZeroSampleTime;
-            return samplesSinceStart / samplesPerBar;
+            return samplesSinceStart / this.samplesPerBar;
         }
 
+        /// <summary>
+        /// This function must be called before any information is recieved through the
+        /// Receive...DuringUpdate() functions.
+        /// </summary>
         public void StartUpdate()
         {
             Debug.Assert(this.CurrentlyUpdating == false);
@@ -85,6 +135,11 @@ namespace MidiShapeShifter.Mss.Relays
             this.ChangedFieldsInLastUpdate = HostInfoFields.None;
         }
 
+        /// <summary>
+        /// Triggers events for subscribers to this relay's output port which will notify them of 
+        /// changes made to the information in this relay. This function should be called after a
+        /// subscriber to this relay's input port is done sending information.
+        /// </summary>
         public void FinishUpdate()
         {
             Debug.Assert(this.CurrentlyUpdating == true);
@@ -96,10 +151,12 @@ namespace MidiShapeShifter.Mss.Relays
                 this.BarPosIsInitialized == true &&
                 this.SampleRateIsInitialized == true)
             {
-                if (this.UpdatedFieldsInLastUpdate.HasFlag(HostInfoFields.Tempo) ||
-                    this.UpdatedFieldsInLastUpdate.HasFlag(HostInfoFields.TimeSignature) ||
-                    this.UpdatedFieldsInLastUpdate.HasFlag(HostInfoFields.BarPos) ||
-                    this.UpdatedFieldsInLastUpdate.HasFlag(HostInfoFields.SampleRate))
+                //If anything has changed that could have changed the position of the calculated 
+                //bar 0 then it will be updated (as will this.samplesPerBar).
+                if (this.ChangedFieldsInLastUpdate.HasFlag(HostInfoFields.Tempo) ||
+                    this.ChangedFieldsInLastUpdate.HasFlag(HostInfoFields.TimeSignature) ||
+                    this.ChangedFieldsInLastUpdate.HasFlag(HostInfoFields.BarPos) ||
+                    this.ChangedFieldsInLastUpdate.HasFlag(HostInfoFields.SampleRate))
                 {
                     double timeSig = (double)this.TimeSignatureNumerator / this.TimeSignatureDenominator;
                     double beatsPerBar = timeSig / (1 / 4d);
