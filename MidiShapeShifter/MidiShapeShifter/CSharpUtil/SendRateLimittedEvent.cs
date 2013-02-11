271e12d91ad92a8d6fe22f76dd19b8ed0aa35a48
Rob McDiarmid <RobIanMcd@gmail.com>
2013-02-10 19:59:45 -0500

    Added utility class for ensuring that an event is not too often.

---
+++ MidiShapeShifter/MidiShapeShifter/CSharpUtil/SendRateLimittedEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MidiShapeShifter.CSharpUtil
+{
+    public class SendRateLimittedEvent<eventParamsType>
+    {
+
+        protected AutoResetEvent paramInfoRecieved;
+        protected Task eventSender;
+
+        protected object eventParamsLock;
+        protected eventParamsType nextEventParams;
+
+        public SendRateLimittedEvent(int minMillsBetweenEvents, Action<eventParamsType> callEvent)
+        {
+            this.paramInfoRecieved = new AutoResetEvent(false);
+            eventParamsLock = new object();
+
+            eventSender = new Task(() => { 
+                while (true) {
+                    this.paramInfoRecieved.WaitOne();
+                    lock (this.eventParamsLock) {
+                        callEvent(this.nextEventParams);
+                    }
+                    Thread.Sleep(minMillsBetweenEvents);
+                }
+            });
+
+            eventSender.Start();
+        }
+
+        public void SendEvent(eventParamsType eventParams) {
+            lock(this.eventParamsLock) {
+                this.nextEventParams = eventParams;
+                this.paramInfoRecieved.Set();
+            }
+        }
+    }
+}
---
+++ MidiShapeShifter/MidiShapeShifterTest/CSharpUtil/sendRateLimittedEventTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using MidiShapeShifter.CSharpUtil;
+using System.Threading;
+
+namespace MidiShapeShifterTest.CSharpUtil
+{
+    class SendRateLimittedEventTest
+    {
+        //I was getting some random failures with this set to 500.
+        protected const int EVENT_WAIT_TIMEOUT = 1000;
+
+        [Test]
+        public void SendEvent_SingleEvent_EventIsSent()
+        {
+            AutoResetEvent eventSent = new AutoResetEvent(false);
+
+            SendRateLimittedEvent<int> limittedEvent = new SendRateLimittedEvent<int>(100, num =>
+            {
+                eventSent.Set();
+            });
+
+            limittedEvent.SendEvent(1);
+
+            bool signalRecieved = eventSent.WaitOne();
+
+            Assert.IsTrue(signalRecieved);
+        }
+
+        [Test]
+        public void SendEvent_multipleEvents_OnlyTheFirstAndLastEventsAreSent()
+        {
+            AutoResetEvent eventSent = new AutoResetEvent(false);
+
+            int numberSetByEvent = -1;
+            SendRateLimittedEvent<int> limittedEvent = new SendRateLimittedEvent<int>(100, num =>
+            {
+                numberSetByEvent = num;
+                eventSent.Set();
+            });
+
+            limittedEvent.SendEvent(1);
+
+            eventSent.WaitOne(1000);
+            Assert.AreEqual(1, numberSetByEvent);
+
+            limittedEvent.SendEvent(2);
+            limittedEvent.SendEvent(3);
+            limittedEvent.SendEvent(4);
+
+            Assert.AreEqual(1, numberSetByEvent);
+
+            limittedEvent.SendEvent(5);
+
+            eventSent.WaitOne(1000);
+            Assert.AreEqual(5, numberSetByEvent);
+
+        }
+
+        [Test]
+        public void SendEvent_multipleEvents_LastEventIsOutputAfterTimeIsUp()
+        {
+            AutoResetEvent eventSent = new AutoResetEvent(false);
+
+            int numberSetByEvent = -1;
+            SendRateLimittedEvent<int> limittedEvent = new SendRateLimittedEvent<int>(100, num =>
+            {
+                numberSetByEvent = num;
+                eventSent.Set();
+            });
+
+            limittedEvent.SendEvent(1);
+
+            eventSent.WaitOne(1000);
+            Assert.AreEqual(1, numberSetByEvent);
+
+            limittedEvent.SendEvent(2);
+            limittedEvent.SendEvent(3);
+            limittedEvent.SendEvent(4);
+
+            Assert.AreEqual(1, numberSetByEvent);
+
+            Thread.Sleep(200);
+
+            Assert.AreEqual(4, numberSetByEvent);
+
+        }
+
+    }
+}
