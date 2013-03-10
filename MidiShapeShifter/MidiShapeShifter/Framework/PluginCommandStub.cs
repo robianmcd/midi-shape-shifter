c49bedfac8c465f1aa12d5dfd23fb660555c5ef0
Rob McDiarmid <RobIanMcd@gmail.com>
2013-03-10 18:24:54 -0400

    Fixed crash that could occur if you deleted some programs andthen opened a previously saved MSS.

--- MidiShapeShifter/MidiShapeShifter/Framework/PluginCommandStub.cs
+++ MidiShapeShifter/MidiShapeShifter/Framework/PluginCommandStub.cs
@@ -1,4 +1,5 @@
-﻿using Jacobi.Vst.Core.Plugin;
+﻿using System;
+using Jacobi.Vst.Core.Plugin;
 using Jacobi.Vst.Framework;
 using Jacobi.Vst.Framework.Plugin;
 
@@ -21,6 +22,23 @@ namespace MidiShapeShifter.Framework
         protected override IVstPlugin CreatePluginInstance()
         {
             return new Plugin();
+        }
+
+        public override void SetProgram(int programNumber)
+        {
+            //When you load an instance of MSS this function will get called. ProgramNumber will be 
+            //set to the index of the program that was selected when you saved MSS. So if some 
+            //programs were added or deleted since MSS was saved then this will be wrong. This try
+            //catch block is a quick fix to prevent an exception from being thrown but it doesn't 
+            //really fix the problem.
+            try
+            {
+                base.SetProgram(programNumber);
+            }
+            catch (ArgumentOutOfRangeException e)
+            { 
+                //If nothing is set when the plugin loads then it will default to the first program which is "Blank"
+            }
         }
     }
 }
