5d823e3f22740adf4c113915eb6b087c92ae3439
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-06-21 01:28:01 -0700

    build: fix and optimize the ILRepack merge

--- MidiShapeShifter/MidiShapeShifter/postbuild.ps1
+++ MidiShapeShifter/MidiShapeShifter/postbuild.ps1
@@ -11,9 +11,16 @@ if (Test-Path "$dist") {
 mkdir "$dist"
 
 # Copy all files except dlls
-ls -r "$env:TargetDir" -Exclude *.dll | cp -r -Force -Destination "$dist"
+ls -r "$env:TargetDir" -Exclude *.dll,*.pdb | cp -r -Force -Destination "$dist"
 
 # Copy the main VST dll
 cp "$env:TargetDir/Jacobi.Vst.Interop.dll" "$dist/$env:TargetFileName"
+
+# Copy VST.net dlls
+cp "$env:TargetDir/Jacobi.Vst.Core.dll" "$dist"
+cp "$env:TargetDir/Jacobi.Vst.Framework.dll" "$dist"
+
+# Debugging symbols
+cp "$env:TargetDir/MidiShapeShifter.net.pdb" "$dist"
 
 ls "$dist"
